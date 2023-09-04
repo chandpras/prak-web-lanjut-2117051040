69a34818f33b0aae44a52bab46af9a5390dc862c
Chandra Prasetya <chandraprasetyap08@gmail.com>
2023-09-04 12:55:44 +0700

    Edit Welcome Page Title

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Chandra Prasetya Putra-2117051040</h1>
 
         <h2>The small framework with powerful features</h2>
 
